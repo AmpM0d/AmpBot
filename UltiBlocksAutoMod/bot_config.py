60ea40987b32fccce02985a5ffe621372e5fded1
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-09 21:36:44 -0600

    Updated config format and updated main script to use the config file. At this point, it might actually work.

--- UltiBlocksAutoMod/bot_config.py
+++ UltiBlocksAutoMod/bot_config.py
@@ -1,17 +1,17 @@
 bots=[]
 
 from .bots.remindmebot import iteration as remindmebot
-bots.append({
+bots["remindmebot"]={
     "name":"remindmebot",
     "prettyname":"Reminder Bot",
     "description":"A bot that reminds users on their talk pages when they add '/remindme' to a talk page",
     "function":remindmebot
-})
+}
 
 from .bots.remindmebot import iteration as sandboxbot
-bots.append({
+bots["sandboxbot"]={
     "name":"sandboxbot",
     "prettyname":"Sandbox Clearing Bot",
     "description":"A bot that clears the global sandbox if no human has edited it for 4 hours",
     "function":sandboxbot
-})
+}
--- UltiBlocksAutoMod/runbots.py
+++ UltiBlocksAutoMod/runbots.py
@@ -1,7 +1,7 @@
 """Helper script to run a select combination of bots"""
 
 # Import necessary modules
-from .bots import sandbox, remind
+from .bot_config import bots
 from .wikisite import site
 import time
 
@@ -17,16 +17,14 @@ def runbots(botlist):
 def main(argv):
     # If no arguments, run all bots.
     if len(argv)==1:
-        bots=[sandbox,remind]
+        botstorun=[bots[i]["function"] for i in bots]
     else:
         # Otherwise run the chosen bots
         # TODO: I'm going to make a more centralized system for managing multiple bots
-        bots=[]
-        if "sandboxbot" in argv:
-            bots.append(sandbox)
-        if "remindbot" in argv:
-            bots.append(remind)
-    runbots(bots)
+        botstorun=[]
+        for item in sys.argv[1:]:
+            botstorun.append(bots[item])
+    runbots(botstorun)
 
 if __name__=="__main__":
     # If this script is being run as main, do the stuff.
