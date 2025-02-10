04408ba75ce342dd4c8fec837bc68cfec4991d78
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-09 20:29:32 -0600

    Comment Windows scripts

--- windows_launch/remindbot.bat
+++ windows_launch/remindbot.bat
@@ -1,2 +1,4 @@
+rem Go to the windows_launch directory
 cd /D "%~dp0"
+rem Run the main file with only remindbot
 start.bat remindbot
--- windows_launch/sandboxbot.bat
+++ windows_launch/sandboxbot.bat
@@ -1,2 +1,4 @@
+rem Go to the windows_launch directory
 cd /D "%~dp0"
+rem Run the main file with only sandboxbot
 start.bat sandboxbot
