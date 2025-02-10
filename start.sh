33e011e4f02e2d57cacdbebc006a6cfa10b6a69e
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-09 21:48:32 -0600

    Now that it ain't broke, I need to fix it.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Go one directory level above this; the project's root directory
-cd "$(dirname $0)"/..
+# Go to the project's root directory
+cd "$(dirname $0)"
 # Run the bot, with any arguments, using the virtual environment's Python.
 .venv/bin/python -m UltiBlocksAutoMod $@
