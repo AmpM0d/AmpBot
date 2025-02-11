ec65ba56d3327d89bdfe1b9e8ef2d1f8bd43b22b
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-10 20:25:19 -0600

    Make it generate the shell scripts. May be ready to merge.

--- UltiBlocksAutoMod/generate_files.py
+++ UltiBlocksAutoMod/generate_files.py
@@ -1,7 +1,13 @@
 import copy
 import json
-import os,sys
+import os,sys,shutil
 os.chdir(os.path.dirname(sys.argv[0])+"/..")
+if os.path.exists("windows"):
+    shutil.rmtree("windows")
+if os.path.exists("linux"):
+    shutil.rmtree("linux")
+os.makedir("windows")
+os.makedir("linux")
 from .bot_config import bots
 def get_text(old,name,prettyname):
     return old.replace("{{name}}",name).replace("{{prettyname}}",prettyname)
@@ -13,3 +19,13 @@ for i in bots.values():
     n["args"].append(i["name"])
     launch_tpl["configurations"].append(n)
 json.dump(launch_tpl,open(".vscode/launch.json","w"))
+
+for name in bots:
+    data=bots["name"]
+    win_tmp=open("templates/bot_win.bat.tpl").read()
+    new=get_text(win_tmp,data["name"],data["prettyname"])
+    open("windows/"+data["name"]+".bat","w").write(new)
+    data=bots["name"]
+    win_tmp=open("templates/bot_linux.sh.tpl").read()
+    new=get_text(win_tmp,data["name"],data["prettyname"])
+    open("linux/"+data["name"]+".sh","w").write(new)
