bd4f0c1402f043c9d361ec8c5e8cd04b73ece6d0
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-28 16:54:25 -0600

    Write code (will probably work)

--- UltiBlocksAutoMod/bots/welcomebot.py
+++ UltiBlocksAutoMod/bots/welcomebot.py
@@ -9,7 +9,19 @@ import pywikibot
 def iteration(runtimevars,iterationvars):
         for e in iterationvars['revisions']:
             print(e)
-            iterationvars["didanything"]=True
+            contribs=pywikibot.User(runtimevars["site"],e["user"]).contributions(len(iterationvars['revisions'])+1)
+            if len(contribs)<=len(iterationvars['revisions']):
+                # Get the user's talk page
+                userpage=pywikibot.Page(runtimevars['site'],"User talk:"+e["user"])
+                # Add the reminder and save the page. Make sure not to mark it as minor,
+                # because then the user won't get notified, which defeats the whole purpose.
+                userpage.text+="\n== Welcome to the AmpMod Wiki! ==\n{{User:UltiBlocksAutoMod/Welcome-v1}} ~~~~"
+                userpage.save("Bot: reminder",minor=False)
+                # Show confirmation that the user has been welcomed
+                # (probably not necessary, but is helpful whenever I'm debugging this)
+                print("Welcomed",e["user"])
+                # Set the flag to update the latest revision checked, and avoid a spam loop.
+                iterationvars["didanything"]=True
 # If we're being run as the main file, print an error,
 # because I don't want to update more entry points than I have to.
 if __name__ == "__main__":
